85fbaa3a905019114d614950b3251c09d3b451ef
Adelia Jupiter <cjadelia@gmail.com>
2024-08-06 15:57:18 -0400

    finished to do

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        public BankAccount() { }
+
+        //In your BankAccount class, create the following:
+        //A private field of type double named balance with a value of 0
+        //Define a method named Deposit that will accept a double and store that value in the balance field
+        //Define a method named GetBalance that will return the amount stored in the balance field
+
+        private double balance = 0;
+
+        public void Deposit(double amount)
+        {
+            balance += amount;
+        }
+
+        public void GetBalance() 
+        { 
+            Console.WriteLine(balance); 
+        }
+
+    }
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Create a new public class named BankAccount
+            //In the main method of your application, create a new instance of the BankAccount class. 
+            //Allow the user of the application to Deposit money and retrieve their balance through the console.
+            var account = new BankAccount();
+            int choice = 0;
+
+            do
+            { 
+                Console.WriteLine("What would you like to do?\n[1] Check Balance\n[2] Deposit\n[3] Exit");
+                choice = Convert.ToInt32(Console.ReadLine());
+          
+                    switch (choice)
+                    {
+                        case 1:
+                            account.GetBalance();
+                            break;
+                        case 2:
+                            Console.WriteLine("Enter deposit amount:");
+                            int amount = Convert.ToInt32(Console.ReadLine());
+                            account.Deposit(amount);
+                            break;
+                        default:
+                            break;
+                    }
+            } while (choice != 3);
         }
     }
 }
